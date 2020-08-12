1e181be4655b70cedd61f7add06624f7b9f7736e
hocastil <Hocastil@ucsc.edu>
2020-08-11 19:50:35 -0700

    Project added

---
+++ amazon_config.py
@@ -0,0 +1,26 @@
+
+from selenium import webdriver
+
+DIRECTORY = 'reports'
+NAME = "PS4"
+CURRENCY = "$"
+MIN_PRICE = "275"
+MAX_PRICE = "650"
+FILTERS = {
+    'min': MIN_PRICE,
+    'max': MAX_PRICE
+}
+BASE_URL = "https://www.amazon.com/"
+
+def get_chrome_web_driver(options):
+    return webDriver.Chrome('./chromedriver.exe', chrome_options=options)
+
+def get_web_driver_options():
+    return webdriver.ChromeOptions()
+
+def set_ignore_certificate_error(options):
+    options.add_argument('--ignore-certificate-errors')
+
+def set_browser_as_incognito(options):
+    options.add_argument('--incognito')
+
