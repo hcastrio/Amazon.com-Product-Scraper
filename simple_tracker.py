7ce095e2057228f10b93f6eaa3ba5220700e16c7
hocastil <Hocastil@ucsc.edu>
2020-08-12 19:17:57 -0700

    now able to open browser, enter product name in search, and scrape date that we need

--- simple_tracker.py
+++ simple_tracker.py
@@ -1,8 +1,128 @@
+from amazon_config import(
+    get_web_driver_options,
+    get_chrome_web_driver,
+    set_ignore_certificate_error,
+    set_browser_as_incognito,
+    NAME,
+    CURRENCY,
+    FILTERS,
+    BASE_URL,
+    DIRECTORY
+)
+
+from selenium.webdriver.common.keys import Keys
+import time
+
+class GenerateReport:
+    def __init__(self):
+        pass
+
+class AmazonAPI:
+    def __init__(self, search_term, filters, base_url, currency):
+        self.base_url = base_url
+        self.search_term = search_term
+        options =  get_web_driver_options()
+        set_ignore_certificate_error(options)
+        set_browser_as_incognito(options)
+        self.driver = get_chrome_web_driver(options)
+        self.currency = currency
+        self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
+        
+    def run(self):
+        print("Starting script...")
+        print(f"Looking for {self.search_term} products...")
+        links = self.get_products_links()
+        time.sleep(1)
+        if not links:
+            print("Stopped script.")
+            return
+        print(f"Got {len(links)} links to products...")
+        print("Getting info about products...")
+        products = self.get_products_info(links)
+
+        self.driver.quit()
+
+    def get_products_info(self, links):
+        asins = self.get_asins(links)
+        products = []
+        for asin in asins:
+            product = self.get_single_product_info(asin)
+
+    def get_single_product_info(self, asin):
+        print(f"Product ID: {asin} - getting data...")
+        product_short_url = self.shorten_url(asin)
+        self.driver.get(f'{product_short_url}?language=en_GB')
+        time.sleep(2)
+        title = self.get_title()
+        seller = self.get_seller()
+        price = self.get_price()
+
+    def get_title(self):
+        try:
+            return self.driver.find_element_by_id('productTitle').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get title of a product - {self.driver.current_url}")
+            return None
+
+    def get_seller(self):
+        try:
+            return self.driver.find_element_by_id('bylineInfo').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get seller of a product - {self.driver.current_url}")
+            return None
+
+    def get_price(self):
+        return '99$'
+
+    # Makes it so the url will only contain the ID
+    def shorten_url(self, asin):
+        return self.base_url + 'dp/' + asin
+
+    def get_asins(self, links):
+        return [self.get_asin(link) for link in links]
+
+    def get_asin(self, product_link):
+        return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
 
 
 
+    
+   
+    def get_products_links(self):
+        # This basically makes the browser load amazon
+        self.driver.get(self.base_url)
+        # This locates the search box
+        element = self.driver.find_element_by_id("twotabsearchtextbox")
+        # This will type our search term into the search bar
+        element.send_keys(self.search_term)
+        # This will press enter
+        element.send_keys(Keys.ENTER)
+        # This waits for the page to load
+        time.sleep(2)
+        self.driver.get(f'{self.driver.current_url}{self.price_filter}')
+        time.sleep(2)
+        result_list = self.driver.find_elements_by_class_name('s-result-list')
+
+        links = []
+        try:
+            results = result_list[0].find_elements_by_xpath(
+                "//div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a")
+            links = [link.get_attribute('href') for link in results] # Pulls all links
+            return links
+        except Exception as e:
+            print("Didn't get any products...")
+            print(e)
+        return links
+
+    
+    
+    
 
 
 
 if __name__ == '__main__':
     print("Hey!!!")
+    amazon = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
+    data = amazon.run()
